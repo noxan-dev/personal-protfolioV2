b2e12a61fd9b519b6c94ab17cfb6340ee7ad60ba
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-07-15 19:28:10 -0400

    removed Instagram API, added upload

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for, request, session
+from werkzeug.utils import secure_filename
 from flask_caching import Cache
 from datetime import datetime
 import os
 import requests
 import smtplib
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+
+photos = UploadSet("photos", IMAGES)
+
+UPLOAD_FOLDER = 'static/images/gallery'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 app = Flask(__name__)
 cache.init_app(app)
+
+app.config["UPLOADED_PHOTOS_DEST"] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+configure_uploads(app, photos)
 
 MAIL_USERNAME = 'chaimmalek@gmail.com'
 MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-
-
-# parmas = {
-#     'client_id': '5596470317044314',
-#     'redirect_uri': 'https://chaimswebsite.herokuapp.com/',
-#     'scope': 'user_profile,user_media',
-#     'response_type': 'code'
-# }
-# response = requests.get('https://api.instagram.com/oauth/authorize', params=parmas)
-# print(response.url)
 
 
 @app.template_filter('remove_dashes')
@@ -52,7 +53,7 @@ def redesign():
                 smtp.login(MAIL_USERNAME, MAIL_PASSWORD)
                 smtp.sendmail(from_addr=email,
                               to_addrs=MAIL_USERNAME,
-                              msg='Subject: New Message from {}\n\n{}\n{}'.format(name, message, email)
+                              msg='Subject: New Message from {}\n\n{}\nFrom: {}'.format(name, message, email)
                               )
             flash('Message sent!')
             return redirect(url_for('redesign'))
@@ -71,6 +72,20 @@ def projects():
     response = requests.get('https://api.github.com/users/noxan-dev/repos')
     repos_json = response.json()
     return render_template('projects.html', repos=repos_json)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        photos.save(request.files['photo'])
+        flash("Photo saved.")
+        return redirect(url_for('redesign'))
+    return render_template('upload.html')
 
 
 if __name__ == '__main__':
